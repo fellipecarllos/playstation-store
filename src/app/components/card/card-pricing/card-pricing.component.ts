cce4cea1b9970de0b353855e7fd3d17aa6ee484d
Felipe Carlos <fellipecarllos@msn.com>
2023-10-07 17:04:54 -0300

    project created

---
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pricing',
+  templateUrl: './card-pricing.component.html',
+  styleUrls: ['./card-pricing.component.css']
+})
+export class CardPricingComponent implements OnInit{
+
+  @Input()
+  gameType:string="TESTETE"
+  @Input()
+  gamePrice:string="RS 399,90"
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
